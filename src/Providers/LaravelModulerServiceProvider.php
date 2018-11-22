d53180e398f25df340b2d0778b8bb848464181a2
rahmanasif05004 <rahmanasif05004@gmail.com>
2018-11-22 22:13:40 +0600

    added initial package service provider

---
+++ src/Providers/LaravelModulerServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace Asif\LaravelModuler\Providers;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelModulerServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        
+    }
+
+    public function register()
+    {
+
+    }
+}
