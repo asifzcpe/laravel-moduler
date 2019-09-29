4ee72db7bccd3bad15eb85cf6fc08ee98eb61d01
Ashraful Islam Tushar <ai.tushar19@gmail.com>
2019-09-29 19:14:36 +0600

    removed less needed variable, updated variabled names to more meaningfull. passing some extra variable to generate controller method

--- src/Traits/FolderGenerateAble.php
+++ src/Traits/FolderGenerateAble.php
@@ -1,74 +1,64 @@
 <?php
+
 namespace Asif\LaravelModuler\Traits;
+
 use File;
 
 trait FolderGenerateAble
 {
     use StubGenerateAble;
-    private $moduleName;
-    private $moduleFolders=[
+    private $moduleFolders = [
         'Controllers',
         'Models',
         'Database',
         'Requests',
         'Routes'
     ];
-    private $databaseSubFolders=['Migrations','Factories','Seeds'];
+    private $databaseSubFolders = ['Migrations', 'Factories', 'Seeds'];
 
-    public function generateModule($moduleName,$authOption=false,$srcFolderName="Modules")
+    public function generateModule($moduleName, $authOption = false, $srcFolderName = "Modules")
     {
-        $this->moduleName=$srcFolderName.'/'.ucfirst($moduleName);
+        $modulePath = $srcFolderName . '/' . ucfirst($moduleName);
 
-        if(!file_exists($this->moduleName))
-        {
-            File::makeDirectory($this->moduleName,$mode=0777,true,true);
-            if($srcFolderName==='Modules')
-            {
-                array_push($this->moduleFolders,"Views");
+        if (!file_exists($modulePath)) {
+            File::makeDirectory($modulePath, $mode = 0777, true, true);
+            if ($srcFolderName === 'Modules') {
+                array_push($this->moduleFolders, "Views");
             }
-            $this->generateModuleFolders($this->moduleName,$this->moduleFolders);
-            $this->generateModuleSubFolders($this->moduleName,'Database',$this->databaseSubFolders);
-            $this->generateController($moduleName,$this->moduleName);
-            $this->generateRequest($moduleName,$this->moduleName);
-            $this->generateModel($moduleName,$this->moduleName);
-            $this->generateMigrationFiles($moduleName,$this->moduleName);
-            if($srcFolderName==="Modules")
-            {
-                $this->generateViewFiles($moduleName,$this->moduleName);
-                $this->generateWebRoute($moduleName,$authOption,$this->moduleName);
+            $this->generateModuleFolders($modulePath, $this->moduleFolders);
+            $this->generateModuleSubFolders($modulePath, 'Database', $this->databaseSubFolders);
+            $this->generateController($moduleName, $modulePath, $srcFolderName);
+            $this->generateRequest($moduleName, $modulePath);
+            $this->generateModel($moduleName, $modulePath);
+            $this->generateMigrationFiles($moduleName, $modulePath);
+            if ($srcFolderName === "Modules") {
+                $this->generateViewFiles($moduleName, $modulePath);
+                $this->generateWebRoute($moduleName, $authOption, $modulePath);
+            } else {
+                $this->generateApiRoute($moduleName, $authOption, $srcFolderName);
             }
-            else
-            {
-                $this->generateApiRoute($moduleName,$authOption,$srcFolderName);
-            }
-        }
-        else
-        {
-            $this->error($moduleName." module has already been created. Please, try other");
+        } else {
+            $this->error($moduleName . " module has already been created. Please, try other");
         }
     }
 
-    private function generateModuleFolders($moduleName,$folders)
+    private function generateModuleFolders($moduleName, $folders)
     {
-        foreach($folders as $moduleFolder)
-        {
-                $folder=$moduleName.'/'.$moduleFolder;
-                if(!file_exists($folder))
-                {
-                    File::makeDirectory($folder,$mode=0777,true,true);
-                }   
+        foreach ($folders as $moduleFolder) {
+            $folder = $moduleName . '/' . $moduleFolder;
+            if (!file_exists($folder)) {
+                File::makeDirectory($folder, $mode = 0777, true, true);
+            }
         }
     }
 
-    private function generateModuleSubFolders($moduleName,$folderName,$subFolders)
+    private function generateModuleSubFolders($moduleName, $folderName, $subFolders)
     {
-        foreach($subFolders as $subFolder)
-        {
-                $folder=$moduleName.'/'.$folderName.'/'.$subFolder;
-                if(!file_exists($folder))
-                {
-                    File::makeDirectory($folder,$mode=0777,true,true);
-                }   
+        foreach ($subFolders as $subFolder) {
+            $folder = $moduleName . '/' . $folderName . '/' . $subFolder;
+            if (!file_exists($folder)) {
+                File::makeDirectory($folder, $mode = 0777, true, true);
+            }
         }
     }
 }
