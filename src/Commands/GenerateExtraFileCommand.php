d1d322a1a0974e6e2d56af6078190f9475de29ce
asifzcpe <rahmanasif05004@gmail.com>
2019-10-21 18:23:26 +0600

    added some functions

--- src/Commands/GenerateExtraFileCommand.php
+++ src/Commands/GenerateExtraFileCommand.php
@@ -4,24 +4,55 @@ use Illuminate\Console\Command;
 use Asif\LaravelModuler\Traits\FolderGenerateAble;
 use Asif\LaravelModuler\Traits\StubGenerateAble;
 use File;
-
+use Artisan;
 class GenerateExtraFileCommand extends Command
 {
     
     private $fileTypes=[
-    	'controller',
-    	'model',
-    	'migration',
-    	'request',
+    	'Controllers',
+    	'Models',
+    	'Migrations',
+    	'Requests',
     ];
-    protected $signature='lmoduler:extra {fileType}';
+    private $modules=[];
+    private $selectedModule;
+
+    private $selectedFileType;
+    protected $signature='lmoduler:extra {ModuleType}';
 
     protected $description="This command is used to generate extra controller,migration,model or request file in a module";
 
     public function handle()
     {
-        $fileTypeChoice=$this->choice('Select Type',$this->fileTypes);
-        $this->info($fileTypeChoice);
+        $this->selectedFileType=$this->choice('Select Type',$this->fileTypes);
+        $this->modules=scandir(base_path('Api/v1/'));
+        $this->selectedModule=$this->choice('Select the module',array_values(array_diff($this->modules,['.','..'])));
+
+        switch($this->selectedFileType)
+        {
+            case 'Controllers':
+            Artisan::call('make:controller',[
+                'name'=>'..\..\..\Api\v1\Device\Controllers\TestController',
+                '--resource'=>'--resource'
+            ]);
+            break;
+
+            case 'Models':
+            $this->info('controllers');
+            break;
+
+            case 'Migrations':
+            $this->info('controllers');
+            break;
+
+            case 'Requests':
+            $this->info('controllers');
+            break;
+
+        }
+
     }
+
+    
     
 }
